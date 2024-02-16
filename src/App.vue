87ee019f0a29477e8f9deb0d47d87b1ef62da9e1
Gowrishankar R <65131244+GSR927@users.noreply.github.com>
2024-02-16 20:30:20 +0530

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <!-- 
-	This is the main page of the application, the layout component is used here,
+        This is the main page of the application, the layout component is used here,
 	and the router-view is passed to it.
 	Layout component is dynamically declared based on the layout for each route,
-	specified in routes list router/index.js .
+	specified in routes list router/index.js.
  -->
 
 <template>
@@ -21,9 +21,9 @@
 			// <layout-default></layout-default> component.
 			layout() {
 				return "layout-" + ( this.$route.meta.layout || "default" ).toLowerCase() ;
-			}
-		},
-	})
+			         }
+		          },
+	                })
 	
 </script>
 
